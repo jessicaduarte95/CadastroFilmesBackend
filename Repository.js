effc3f0aefdb3619c89915efb1e3f0514037670e
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-08-31 18:15:54 -0300

    Ajustes - inserir filmes

--- Repository.js
+++ Repository.js
@@ -1,17 +1,17 @@
 const Cadastro = require('./model');
 
 const inserirFilmes = async (name, year, category, sinopse) => {
-    // try {
-    //     const insert =  await Cadastro.create({
-    //         title: name,
-    //         year: year,
-    //         category: category,
-    //         sinopse: sinopse
-    //     })
-    //     console.log(insert);
-    // } catch (error) {
-    //     console.log(error);
-    // }
+    try {
+        const insert =  await Cadastro.create({
+            title: name,
+            year: year,
+            category: category,
+            sinopse: sinopse
+        })
+        console.log(insert);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
