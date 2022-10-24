9a4f3ba78b6b64e12560c2bc2e6f0099498afd4f
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-10-24 20:02:48 -0300

    Implementação - Swagger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Filmes = require("./Controller");
+const Filmes = require("./controller");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require('./swagger.json');
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.post("/cadastrar", Filmes.inserirFilmes);
 
@@ -13,6 +16,12 @@ app.get("/filmesCadastrados", Filmes.obterFilmes);
 app.delete("/deletarFilme/:id", Filmes.deletarFilmes);
 
 app.put("/editarfilme/:id", Filmes.editarFilmes)
+
+app.get("/terms", (req,res) => {
+    return res.json({
+        message: "Termos de Serviço."
+    });
+});
 
 app.get('/', (req,res) => {
     res.send("Teste");
